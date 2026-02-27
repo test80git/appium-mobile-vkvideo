1312923cd03b1b9e0be5c3bc5c738059fe38ea0f
test80git <test80git@yandex.ru>
2026-02-27 10:00:29 +0300

    dev Добавлен Tests

---
+++ src/test/java/ru/kuz/tests/BaseTest.java
@@ -0,0 +1,63 @@
+package ru.kuz.tests;
+
+import static ru.kuz.helper.Constants.SCREENSHOT_TO_SAVE_FOLDER;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.net.MalformedURLException;
+
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import ru.kuz.helper.RunHelper;
+import ru.kuz.listeners.AllureListener;
+
+
+/**
+ * Базовый тестовый класс
+ */
+@ExtendWith(AllureListener.class)//расширяем тестовый класс
+public class BaseTest {
+
+
+    @BeforeEach
+    public void setup() throws MalformedURLException {
+        // Логирование для Allure
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        // папка для сохранения скриншотов selenide
+        Configuration.reportsFolder = SCREENSHOT_TO_SAVE_FOLDER;
+
+        //инициализируем андройд драйвер
+        Configuration.browser = RunHelper.runHelper().getDriverClass().getName();
+        Configuration.browserSize = null;
+        Configuration.timeout = 10000;
+//        disableAnimationOnEmulator();
+
+    }
+
+//    /**
+//     * Отключение анимаций на эмуляторе чтобы не лагало
+//     */
+//    private static void disableAnimationOnEmulator() {
+//        executeBash("adb -s shell settings put global transition_animation_scale 0.0");
+//        executeBash("adb -s shell settings put global window_animation_scale 0.0");
+//        executeBash("adb -s shell settings put global animator_duration_scale 0.0");
+//    }
+
+
+    @BeforeEach
+    public void startDriver() {
+        Allure.step("Открыть приложение", () -> Selenide.open());
+    }
+
+    @AfterEach
+    public void afterEach() {
+        Allure.step("Закрыть приложение", Selenide::closeWebDriver);
+    }
+
+}
---
+++ src/test/java/ru/kuz/tests/VkVideoTests.java
@@ -0,0 +1,44 @@
+package ru.kuz.tests;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.qameta.allure.Description;
+import ru.kuz.pages.MainPage;
+import ru.kuz.pages.VideoPlayerPage;
+
+/**
+ * Тесты для VK Video
+ */
+public class VkVideoTests extends BaseTest {
+
+    private MainPage mainPage;
+    private VideoPlayerPage playerPage;
+
+    @BeforeEach
+    public void initPages() {
+        mainPage = new MainPage();
+        playerPage = new VideoPlayerPage();
+    }
+
+    @Test
+    @Description("Позитивный тест: Видео успешно воспроизводится")
+    public void testVideoPlaybackPositive() {
+        // ШАГ 1: Открываем видео
+        mainPage.waitForPageLoaded()
+                .openFirstVideo();
+
+        // ШАГ 2: Ждем загрузки плеера
+        playerPage.waitForPlayerLoaded();
+        // ШАГ 3: Тапаем по видео (появляются кнопки)
+        playerPage.tapOnVideo();
+        // ШАГ 4: Ждем появления кнопок
+        playerPage.wait(5);
+        // ШАГ 5: Тап вверху (скрываем кнопки)
+        playerPage.tapOnTop();
+        playerPage.wait(2);
+        // ШАГ 6: Проверяем что время увеличивается
+        playerPage.verifyTimeIncreasing();
+    }
+
+}
